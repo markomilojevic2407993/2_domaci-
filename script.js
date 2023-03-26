7ddfc0529155998ffd54c13efd5db741ad920c8b
markomilojevic2407993 <markomilojevic2407993@gmail.com>
2023-03-26 21:14:07 +0200

    changes script

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function promeni(){
-    let dugmad=document.querySelectorAll('.dugme');
-    for(var i=0; i<dugmad.length; i++){
-    dugmad[i].innerHTML='in your cash';
+function red(){
+    let dugmeCrveno=document.getElementById('crvenoDugme');
+    dugmeCrveno.innerHTML='in your bill'
+        
 }
+
+function blue(){
+    let dugmePlavo=document.getElementById('dugmePlavo');
+    dugmePlavo.innerHTML='in your bill'
+        
+}
+
+
+function yelow(){
+    let dugmeZuto=document.getElementById('dugmeZuto');
+    dugmeZuto.innerHTML='in your bill'
         
 }
